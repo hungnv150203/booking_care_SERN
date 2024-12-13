0c2fe3169da87fa7d5a9c728fd746825699f074e
hungnv150203 <vuhunghs2@gmail.com>
2024-12-13 10:45:48 +0700

    FE: optimize

--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -16,6 +16,7 @@ import System from '../routes/System';
 import HomePage from './HomePage/HomePage';
 
 import { CustomToastCloseButton } from '../components/CustomToast';
+import CustomScrollbars from '../components/CustomScrollbars';
 
 class App extends Component {
 
@@ -43,14 +44,16 @@ class App extends Component {
                 <Router history={history}>
                     <div className="main-container">
                         {this.props.isLoggedIn && <Header />}
-                        <span className="content-container">
-                            <Switch>
-                                <Route path={path.HOME} exact component={(Home)} />
-                                <Route path={path.LOGIN} component={userIsNotAuthenticated(Login)} />
-                                <Route path={path.SYSTEM} component={userIsAuthenticated(System)} />
-                                <Route path={path.HOMPAGE} component={HomePage} />
-                            </Switch>
-                        </span>
+                        <div className="content-container">
+                            <CustomScrollbars style={{ height: '100vh', witdh: '100%' }}>
+                                <Switch>
+                                    <Route path={path.HOME} exact component={(Home)} />
+                                    <Route path={path.LOGIN} component={userIsNotAuthenticated(Login)} />
+                                    <Route path={path.SYSTEM} component={userIsAuthenticated(System)} />
+                                    <Route path={path.HOMPAGE} component={HomePage} />
+                                </Switch>
+                            </CustomScrollbars>
+                        </div>
 
                         <ToastContainer
                             className="toast-container" toastClassName="toast-item" bodyClassName="toast-item-body"
