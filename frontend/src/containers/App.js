b463767169e61aab56bc249d33e616eaa4bb35fc
hungnv150203 <vuhunghs2@gmail.com>
2025-01-06 11:33:45 +0700

    FE: optimized

--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -53,7 +53,7 @@ class App extends Component {
 
                         <ToastContainer
                             position="bottom-right"
-                            autoClose={5000}
+                            autoClose={3000}
                             hideProgressBar={false}
                             newestOnTop={false}
                             closeOnClick={false}
